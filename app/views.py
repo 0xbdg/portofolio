61a663157f88f5d5d38664e447b53979c099bae1
?0 <benjamindguntara@protonmail.com>
2024-03-13 11:01:27 +0700

    whijidhudqw

--- app/views.py
+++ app/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,"pages/index.html")
+
+def article(request):
+    return render(request,"pages/article.html")
+
+def video(request):
+    return render(request,"pages/video_player.html")
