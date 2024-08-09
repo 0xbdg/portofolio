d9ab952aaf2fcfc6420fb19c431307f1bb4e3986
?0 <benjamindguntara@protonmail.com>
2024-08-09 22:06:22 +0700

    dasdjasdjlaskjdlakjd

--- portofolio/urls.py
+++ portofolio/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('portfolio-admin/', admin.site.urls),
     path("",include("app.urls")),
     path("__reload__/", include("django_browser_reload.urls"))
 ]
